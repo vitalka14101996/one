bc126e37026a0ca9abb9b167f20679c39e8bd43e
Khalton_dz <155322287+vitalka14101996@users.noreply.github.com>
2024-09-04 00:20:32 +0100

    Delete server directory

--- server/src/main/java/com/genymobile/scrcpy/device/ConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package com.genymobile.scrcpy.device;
-
-public class ConfigurationException extends Exception {
-    public ConfigurationException(String message) {
-        super(message);
-    }
-}
